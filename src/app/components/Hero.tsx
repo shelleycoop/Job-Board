8271b03e886d221b7bbb8c7ce4c5d6f0a0d57180
Sanjeev Kumar <89408233+shelleycoop@users.noreply.github.com>
2024-06-19 11:51:41 +0530

    Homepage 1

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,21 @@
 export default function Hero (){
     return (
-    <section> hero </section>
+    <section className="py-12"> 
+        <h1 className="text-4xl font-bold text-center">
+            Find Your Next <br/> Dream Job
+        </h1>
+        <p className="text-center text-gray-600">Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Doloribus repellat error expedita, doloremque labore eum 
+            sapiente commodi natus in magnam iste? Modi enim, vero impedit quos sequi 
+            itaque magnam sed?</p>
+        <form>
+            <input 
+             type="search" 
+             className="border border-gray-400 w-full py-2 px-3 rounded-md" 
+             placeholder="Search Phrase..."/>
+
+            <button className="bg-blue-600 text-white py-2 px-4 rounded-md">Search</button>
+        </form>    
+    </section>
 )
 }
