466582b565ba535d11de3ceb8e90b88639a492eb
Sanjeev Kumar <89408233+shelleycoop@users.noreply.github.com>
2024-06-19 12:47:46 +0530

    Homepage 3

--- src/app/components/Jobs.tsx
+++ src/app/components/Jobs.tsx
@@ -1,7 +1,7 @@
 export default function Jobs() {
     return (
-        <>
+        <div className="bg-gray-100 p-8 rounded-xl">
             <h2>Recent Jobs</h2>
-        </>
+        </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="py-4 px-6 container mx-auto">
+        <main className="container">
           <Header/>
           {children}
         </main>
